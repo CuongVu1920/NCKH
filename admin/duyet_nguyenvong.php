03fe3bf7b0629b71a8a84ced945a3d866f068d6c
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-02-24 22:36:40 +0700

    giới hạn duyệt nguyện vọng

--- admin/duyet_nguyenvong.php
+++ admin/duyet_nguyenvong.php
@@ -102,38 +102,72 @@ $result = mysqli_query($conn, $sql);
                     </tr>
                 </thead>
                 <tbody>
-                    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                    <?php while ($row = mysqli_fetch_assoc($result)) {
+                        // Mảng chứa tất cả các nguyện vọng
+                        $nguyenvong = [
+                            1 => ['name' => $row['ngv_1'], 'id' => $row['id_ngv_1']],
+                            2 => ['name' => $row['ngv_2'], 'id' => $row['id_ngv_2']],
+                            3 => ['name' => $row['ngv_3'], 'id' => $row['id_ngv_3']],
+                        ];
+
+                        // Kiểm tra số lượng sinh viên của từng giảng viên
+                        $can_choose = [];
+                        foreach ($nguyenvong as $key => $value) {
+                            $sql_check_count = "SELECT COUNT(*) AS student_count 
+                                                FROM huongdan 
+                                                WHERE id_giangvien = ?";
+                            $stmt_check = mysqli_prepare($conn, $sql_check_count);
+                            mysqli_stmt_bind_param($stmt_check, 'i', $value['id']);
+                            mysqli_stmt_execute($stmt_check);
+                            mysqli_stmt_bind_result($stmt_check, $student_count);
+                            mysqli_stmt_fetch($stmt_check);
+                            mysqli_stmt_close($stmt_check);
+                            $can_choose[$key] = $student_count < 5;
+                        }
+                    ?>
                         <tr>
-                            <td class="student-name" rowspan="3"> <?php echo $row['sinhvien']; ?> </td>
-                            <td>Nguyện vọng 1</td>
-                            <td><?php echo $row['ngv_1'] ? $row['ngv_1'] : 'Không có'; ?></td>
-                            <td rowspan="3">
-                                <select name="approved_<?php echo $row['id_sinhvien']; ?>" class="approve-select">
-                                    <option value="0">Không chấp nhận</option>
-                                    <?php if ($row['id_ngv_1']) { ?>
-                                        <option value="<?php echo $row['id_ngv_1']; ?>"><?php echo $row['ngv_1']; ?></option>
-                                    <?php } ?>
-                                    <?php if ($row['id_ngv_2']) { ?>
-                                        <option value="<?php echo $row['id_ngv_2']; ?>"><?php echo $row['ngv_2']; ?></option>
-                                    <?php } ?>
-                                    <?php if ($row['id_ngv_3']) { ?>
-                                        <option value="<?php echo $row['id_ngv_3']; ?>"><?php echo $row['ngv_3']; ?></option>
-                                    <?php } ?>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Nguyện vọng 2</td>
-                            <td><?php echo $row['ngv_2'] ? $row['ngv_2'] : 'Không có'; ?></td>
-                        </tr>
-                        <tr>
-                            <td>Nguyện vọng 3</td>
-                            <td><?php echo $row['ngv_3'] ? $row['ngv_3'] : 'Không có'; ?></td>
+                            <td class="student-name" rowspan="3"><?php echo $row['sinhvien']; ?></td>
+                            <?php 
+                                // Dùng vòng for để hiển thị các nguyện vọng
+                                for ($i = 1; $i <= 3; $i++) { 
+                                    $ngv_name = $nguyenvong[$i]['name'] ? $nguyenvong[$i]['name'] : 'Không có';
+                                    $ngv_id = $nguyenvong[$i]['id'];
+                                    $can_choose_ngv = $can_choose[$i];
+                                    if ($i > 1) {
+                                        echo "<tr>";
+                                    }
+                            ?>
+                                <td>Nguyện vọng <?php echo $i; ?></td>
+                                <td><?php echo $ngv_name; ?></td>
+                                <?php if ($i == 1) { ?>
+                                    <td rowspan="3">
+                                        <select name="approved_<?php echo $row['id_sinhvien']; ?>" class="approve-select">
+                                            <option value="0">Không chấp nhận</option>
+                                            <?php 
+                                                for ($j = 1; $j <= 3; $j++) {
+                                                    $ngv_id = $nguyenvong[$j]['id'];
+                                                    if ($ngv_id && $can_choose[$j]) {
+                                                        echo "<option value='$ngv_id'>{$nguyenvong[$j]['name']}</option>";
+                                                    }
+                                                }
+                                            ?>
+                                        </select>
+                                    </td>
+                                <?php } ?>
+                            <?php 
+                                    if ($i > 1) {
+                                        echo "</tr>";
+                                    }
+                                }
+                            ?>
                         </tr>
                     <?php } ?>
                 </tbody>
             </table>
             <button type="submit" class="btn-submit">Duyệt Nguyện Vọng</button>
+            <?php if (isset($_GET['success']) && $_GET['success'] == '1'): ?>
+                <div class="alert alert-success">Nguyện vọng đã được duyệt thành công!</div>
+            <?php endif; ?>
         </form>
     </div>
 </body>
